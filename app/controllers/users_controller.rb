ed01ab99883dca9344b99b3e166ed0e057c65872
iba83 <sson.joer@icloud.com>
2020-04-30 16:17:05 +0900

    Add_FollowFunction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.search_posts(params[:id])
+    @posts = Post.search_posts(params[:id]).includes(:comments)
     @group_id = @posts.pluck(:group_id)
     @groups = Group.where(id: @group_id)
+    # @userGroupPost = Post.where(user_id: params[:id], group_id: 1 )
+    
+
   end
 
 end
